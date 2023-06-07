41fd2f4d275de43738afc62fb1a6a997733b9129
raselmahmud22 <raselmahmud454b@gmail.com>
2023-06-08 02:42:52 +0800

    init the front end

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import '@fontsource/titillium-web/200.css';
+import '@fontsource/titillium-web/300.css';
+import '@fontsource/titillium-web/400.css';
+import '@fontsource/titillium-web/600.css';
+import '@fontsource/titillium-web/700.css';
+import '@fontsource/titillium-web/900.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
